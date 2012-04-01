d9bfacfe0ebb994caeaa29c132c469b75fcb827e
Magnus P White <mpw@ecs.soton.ac.uk>
2012-04-01 23:08:25 +0100

    v3.0 - upgrade of everything: clean code, more admin options, includes English, Arabic and Marketplace toolbar support

--- functions/functions.php
+++ functions/functions.php
@@ -9,14 +9,15 @@ function atbar_css() {
 
 function atbar_register_settings(){
 
-	register_setting('atbar_options', 'atbar_language');
+	register_setting('atbar_options', 'atbar_version');
+	register_setting('atbar_options', 'atbar_marketplace_toolbar');
 	register_setting('atbar_options', 'atbar_persistent');
 	register_setting('atbar_options', 'atbar_exclude');
 	register_setting('atbar_options', 'atbar_exclude_pages');
-	register_setting('atbar_options', 'atbar_no_show_banner_top');
-	
-	//default language setting is en
-	
+	register_setting('atbar_options', 'atbar_launcher_exclude');
+	register_setting('atbar_options', 'atbar_launcher_exclude_pages');
+	register_setting('atbar_options', 'atbar_launcher_image');	
+
 }
 
 function atbar_add_options(){
@@ -24,53 +25,13 @@ function atbar_add_options(){
 	add_options_page('Atbar', 'Atbar', 'manage_options', 'atbaroptions', 'atbar_options');
 }
 
-function is_language($value) {
+function is_version($value) {
 
-	$language_option = get_option('atbar_language');
+	$version = get_option('atbar_version');
 
-	if($language_option == $value) {
+	if($version == $value) {
 		echo ("selected");
 	}
-}
-
-function persistentlaunch() {
-	
-	$language_option = get_option('atbar_language');
-	
-	$persistenttoolbar = file_get_contents(dirname(__FILE__).'/atbar-launcher-'.$language_option.'.js');
-	
-	switch ($language_option){
-
-		default:
-			echo ('<script language="javascript">');
-				file_get_contents(dirname(__FILE__).'/atbar-launcher-en.js');
-			echo ('</script>');
-		break;
-
-		case "en":
-			echo ('<script language="javascript">'.$persistenttoolbar.'</script>');
-		break;
-
-		case "ar":
-			echo ('<script language="javascript">'.$persistenttoolbar.'</script>');
-		break;
-	}
-
-}
-
-function toolbarlauncher() {
-	
-	$language_option = get_option('atbar_language');
-	
-	$toolbar = addslashes(file_get_contents(dirname(__FILE__).'/atbar-launcher-'.$language_option.'.php'));
-	echo ('<script language="javascript">
-
-	toolbarholder = document.createElement("div");
-	toolbarholder.id = "toolbar-holder";
-	toolbarholder.innerHTML = "'.$toolbar.'";
-	document.body.insertBefore(toolbarholder, document.body.firstChild);
-
-	</script>');
 }
 
 function is_persistent($value) {
@@ -82,9 +43,9 @@ function is_persistent($value) {
 	}
 }
 
-function is_exclude() {
+function is_exclude($setting) {
 
-	$exclude_option = get_option('atbar_exclude');
+	$exclude_option = get_option($setting);
 
 	if($exclude_option == "on") {
 		echo ('checked');
@@ -94,76 +55,166 @@ function is_exclude() {
 	}
 }
 
-function create_exclude_array() {
+function is_banner_show($value) {
 
-	$exclude_pages_option = get_option('atbar_exclude_pages');
-	$exclude_array = explode (',', $exclude_pages_option);
+	$launcher_image_option = get_option('atbar_launcher_image');
+
+	if($launcher_image_option == $value) {
+		echo ("selected");
+	}
+}
+
+function add_toolbar(){
+	
+	$launcher_image_option = get_option('atbar_launcher_image');
+	$persistent_option = get_option('atbar_persistent');
+	
+	// adds persistent toolbar
+	if ($persistent_option == "Yes"){
+		exclude_pages('atbar_exclude', 'atbar_exclude_pages', 'persistentlaunch');
+	}
+	// adds toolbar launcher
+	elseif ($launcher_image_option == "Yes"){
+		exclude_pages('atbar_launcher_exclude', 'atbar_launcher_exclude_pages', 'toolbarlauncher');
+	}
+	else {
+	
+	}
+}
+
+function toolbarlauncher() {
+	
+	$version = get_option('atbar_version');
+	
+	switch ($version){
+
+		default:
+			$js = file_get_contents(dirname(__FILE__).'/atbar-launcher-en.js');
+		break;
+
+		case "en":
+			$js = file_get_contents(dirname(__FILE__).'/atbar-launcher-en.js');
+		break;
+
+		case "ar":
+			$js = file_get_contents(dirname(__FILE__).'/atbar-launcher-ar.js');
+		break;
+		
+		case "marketplace":
+			$js = get_option('atbar_marketplace_toolbar');
+		break;
+	}
+	
+	$launcher = '<a href="'.$js.'" id="toolbar-launch" title="Launch ATbar to adjust this webpage, have it read aloud and other functions."><img src="http://access.ecs.soton.ac.uk/ToolBar/content/toolbar/toolbarlauncher.png" alt="ATbar"></a>';
+	$launcher = addslashes($launcher);
+	
+	echo ('<script language="javascript">
+		toolbarholder = document.createElement("div");
+		toolbarholder.id = "toolbar-holder";
+		toolbarholder.innerHTML = "'.$launcher.'";
+		document.body.insertBefore(toolbarholder, document.body.firstChild);
+		</script>');
+}
+
+function persistentlaunch() {
+	
+	$version = get_option('atbar_version');
+	
+	switch ($version){
+
+		default:
+			$ptoolbar = file_get_contents(dirname(__FILE__).'/atbar-launcher-en.js');
+		break;
+
+		case "en":
+			$ptoolbar = file_get_contents(dirname(__FILE__).'/atbar-launcher-en.js');
+		break;
+
+		case "ar":
+			$ptoolbar = file_get_contents(dirname(__FILE__).'/atbar-launcher-ar.js');
+		break;
+		
+		case "marketplace":
+			$ptoolbar = get_marketplace_toolbar(get_option('atbar_marketplace_toolbar'));
+			
+			if($ptoolbar == NULL){
+				$ptoolbar = file_get_contents(dirname(__FILE__).'/atbar-launcher-en.js');
+			}
+		break;
+	}
+
+	echo ('<script language="javascript">'.$ptoolbar.'</script>');
+
+}
+
+function get_marketplace_toolbar($js) {
+
+	// gets js file of toolbar from the marketplace
+	$js = strchr($js,"http");
+	$js = explode(".user.js",$js);
+	$js = file_get_contents($js[0].'.user.js');
+	
+	return $js;	
+}
+
+function create_exclude_array($pages) {
+	
+	$pages = get_option($pages);
+	$exclude_array = explode (',', $pages);
 
 	return $exclude_array;
 }
 
-function add_toolbar(){
+function exclude_pages($setting, $pages, $function) {
 
-	$no_show_banner_top_option = get_option('atbar_no_show_banner_top');
-
-	// if only widget wants to be selcted don't show banner at top
-	if ($no_show_banner_top_option == "Yes"){
-
-		// is persistent setting on?
-		$persistent_option = get_option('atbar_persistent');
-
-		// adds persistent toolbar
-		if($persistent_option == "Yes") {
-			exclude_pages();
-		}
-
-		// else adds toolbar launcher
-		else {
-			add_action('get_footer', 'toolbarlauncher');
-		}
-	}
-}
-
-function exclude_pages() {
-
-	$array = create_exclude_array();
+	$array = create_exclude_array($pages);
 	$postid = strval(get_the_ID());
-	$exclude_option = get_option('atbar_exclude');
+	$exclude_option = get_option($setting);
 
 	if($exclude_option == "on") {
 
 		if(in_array($postid, $array)) {
-			add_action('get_footer', 'toolbarlauncher');
+
 		}
 		else {
-			add_action('get_footer', 'persistentlaunch');
+			add_action('get_footer', $function);
 		}
 	}
 	else {
-		add_action('get_footer', 'persistentlaunch');
+		add_action('get_footer', $function);
 	}
 }
 
-function banner_show($value) {
-
-	$no_show_banner_top_option = get_option('atbar_no_show_banner_top');
-
-	if($no_show_banner_top_option == $value) {
-		echo ("selected");
-	}
-}
 
 function atbar_widget_init() {
 	register_sidebar_widget(__('ATbar'), 'add_atbar_widget');
 }
 
 function atbar_widget() {
+	
+	$version = get_option('atbar_version');
+	
+	switch ($version){
 
-	$language_option = get_option('atbar_language');	
+		default:
+			$js = file_get_contents(dirname(__FILE__).'/atbar-launcher-en.js');
+		break;
+
+		case "en":
+			$js = file_get_contents(dirname(__FILE__).'/atbar-launcher-en.js');
+		break;
+
+		case "ar":
+			$js = file_get_contents(dirname(__FILE__).'/atbar-launcher-ar.js');
+		break;
+		
+		case "marketplace":
+			$js = get_option('atbar_marketplace_toolbar');
+		break;
+	}
 	
-	$toolbar = file_get_contents(dirname(__FILE__).'/atbar-launcher-'.$language_option.'.php');
-	
-	echo '<div id="toolbar-widget">'.$toolbar.'</div>';	
+	$launcher = '<a href="'.$js.'" id="toolbar-launch" title="Launch ATbar to adjust this webpage, have it read aloud and other functions."><img src="http://access.ecs.soton.ac.uk/ToolBar/content/toolbar/toolbarlauncher.png" alt="ATbar"></a>';
+	echo '<div id="toolbar-widget">'.$launcher.'</div>';	
 }
 
 function add_atbar_widget($args) {
--- options.php
+++ options.php
@@ -3,97 +3,103 @@
 function atbar_options()
 {
 	
-	$language_option = get_option('atbar_language');
+	$version_option = get_option('atbar_version');
+	$marketplace_toolbar_option = get_option('atbar_marketplace_toolbar');
 	$persistent_option = get_option('atbar_persistent');
 	$exclude_option = get_option('atbar_exclude');
 	$exclude_pages_option = get_option('atbar_exclude_pages');
-	$no_show_banner_top_option = get_option('atbar_no_show_banner_top');
+	$exclude_option = get_option('atbar_launcher_exclude');
+	$exclude_launcher_pages_option = get_option('atbar_launcher_exclude_pages');
+	$atbar_launcher_image_option = get_option('atbar_launcher_image');
 	
-	// display options page
-	echo ('<div class="wrap">
+?>
+	
+	<div class="wrap">
 	<div id="icon-options-general" class="icon32"><br /></div>
-	<h2>Atbar Options</h2>
-
-	<form method="post" action="options.php">');
-	settings_fields('atbar_options');
+	<h2>Atbar Options</h2>	
 	
-	//select language
-	echo ('
-	<table class="form-table">
-				<tr valign="top">
-			<th scope="row">Select language:</th>
-			<td>
-				<select name="atbar_language" width="60px">
-					<option value="en" ');
-												echo is_language("en");	echo ('>English</option>
-					<option value="ar" '); echo is_language("ar");	echo ('>Arabic</option>
-				</select>
-			</td>
-		</tr>');
+	<form method="post" action="options.php">
+	<? settings_fields("atbar_options"); ?>
 	
-	//load Atbar at top of page
-	echo ('	
-		<tr valign="top">
-			<th scope="row">Load ATbar launcher at the top of the page? (select no for when using the ATbar widget)</th>
+	<p><b>For instructions on this plugin view the <a href="http://www.atbar.org/wordpress-plugin-guide">ATbar Wordpress Plugin Guide</a>.</b></p>
+	
+	<p>The ATbar sidebar widget can be added in Appearance, Widgets then adding ATbar to your active Widgets.</p>
+	
+	
+	<table class="form-table" style="border-top: 1px solid #DFDFDF;">
+	
+		<!-- select version to use -->		
+		<tr>
+			<th scope="row">Select version:</th>
 			<td>
-				<select name="atbar_no_show_banner_top" width="60px">
-					<option value="No" ');	// checks whether no show banner top is yes or no
-											// sets drop down box to setting selected
-											echo banner_show("No");	echo ('>No</option>
-					<option value="Yes" '); echo banner_show("Yes");	echo ('>Yes</option>
+				<select name="atbar_version">
+					<option value="marketplace"	<? echo is_version("marketplace");	?>>Toolbar from ATbar Marketplace</option>
+					<option value="en"	<? echo is_version("en"); ?>>English</option>
+					<option value="ar" <? echo is_version("ar"); ?>>Arabic</option>
 				</select>
+				
+				<p>Create your own toolbar at <a href="http://marketplace.atbar.org">http://marketplace.atbar.org</a> or use one of the pre-made toolbars.</p>
 			</td>
-		</tr>');
-		
-		if ($no_show_banner_top_option == "No") {
-			echo ('
-			<tr valign="top">
-				<td>
-					<div class="error fade">
-					<p><b>ATbar launcher image at the top of the page is turned off.</b></p>
-					</div>
-				</td>
-			</tr>');
-		}
-		
-		//persistent options
-		echo ('
-		<tr valign="top">
-			<th scope="row">Is ATbar persistant on all pages:</th>
-			<td><b>'.$persistent_option.'</b></td>
+		</tr>
+			<th>Toolbar from ATbar Marketplace:</th>
+			<td>
+				<input type="text" name="atbar_marketplace_toolbar" value="<? echo $marketplace_toolbar_option ?>" style="width:350px;">
+				<p>Copy the install button link in the text box (the full javascript string)</p>
+				<p>For further instructions view the <a href="http://www.atbar.org/wordpress-plugin-guide">guide</a>.</p>
+			</td>
 		</tr>
 		
-		<tr valign="top">
-			<th scope="row">Load ATbar on all pages?</th>
+		<!-- load atbar launcher -->		
+		<tr style="border-top: 1px solid #DFDFDF;">
+			<th scope="row">Load ATbar launcher at the top of the page? (select <b>no</b> for when using the ATbar widget)</th>
 			<td>
-				<select name="atbar_persistent" width="60px">
-					<option value="No" ');	// checks whether persistent option is yes or no
-											// sets drop down box to setting selected
-											echo is_persistent("No");	echo ('>No</option>
-					<option value="Yes" '); echo is_persistent("Yes");	echo ('>Yes</option>
+				<select name="atbar_launcher_image">
+						<option value="No" <? echo is_banner_show("No"); ?>>No</option>
+						<option value="Yes" <? echo is_banner_show("Yes");	?>>Yes</option>
 				</select>
 			</td>
-		</tr>');
+		</tr>
 		
-		//exclude pages
-		echo ('
+		<!-- exclude pages for laucher -->
 		<tr>
-			<th scope="row">Exclude pages?</th>
-			<td><input type="checkbox" name="atbar_exclude" lable="exclude pages" '); is_exclude();
-			
-		echo ('			
-			>Exclude pages</td>
+			<th scope="row">Exclude pages for ATbar launcher?</th>
+			<td><input type="checkbox" name="atbar_launcher_exclude" lable="exclude pages for launcher" <? is_exclude('atbar_launcher_exclude'); ?>>  Exclude pages for ATbar launcher</td>
 		</tr>
 				
 		<tr>
 			<td></td>
-			<td><input type="text" name="atbar_exclude_pages" value="'.$exclude_pages_option.'" style="width:346px;">
-			<br />
-			Enter a comma-separated list of page IDs to exclude, e.g. 1,2,3</td>
-		</tr>	');
+			<td>
+				<input type="text" name="atbar_launcher_exclude_pages" value="<? echo $exclude_launcher_pages_option; ?>" style="width:346px;">
+				<p>Enter a comma-separated list of page IDs or name to exclude, e.g. 1,2,3 etc.</p>
+			</td>
+		</tr>
 		
-		//save changes
-		echo ('
+		<!-- auto load atbar -->		
+		<tr style="border-top: 1px solid #DFDFDF;">
+			<th scope="row">Auto load ATbar on all pages? (user doesn't have to select to load each time)</th>
+			<td>
+				<select name="atbar_persistent">
+					<option value="No" <? echo is_persistent("No"); ?>>No</option>
+					<option value="Yes" <? echo is_persistent("Yes"); ?>>Yes</option>
+				</select>
+			</td>
+		</tr>
+		
+		<!-- exclude pages for auto loader -->
+		<tr>
+			<th scope="row">Exclude pages for auto load?</th>
+			<td><input type="checkbox" name="atbar_exclude" lable="exclude pages for auto loader" <? is_exclude('atbar_exclude'); ?>>  Exclude pages for auto load</td>
+		</tr>
+				
+		<tr>
+			<td></td>
+			<td>
+				<input type="text" name="atbar_exclude_pages" value="<? echo $exclude_pages_option; ?>" style="width:346px;">
+				<p>Enter a comma-separated list of page IDs or name to exclude, e.g. 1,2,3 etc.</p>
+			</td>
+		</tr>
+		
+		<!-- save form -->		
 		<tr valign="top">
 			<th scope="row">
 				<p class="submit">
@@ -101,18 +107,16 @@ function atbar_options()
 				</p>
 			</th>
 		</tr>
-		</form>');
+		</form>
 		
-		
-		echo ('		
-		<br />
-		
-		<form method="post" action="options.php">');
-		settings_fields('atbar_options');
-		echo('
-		<input type="hidden" name="atbar_persistent" value="'.get_option('atbar_persistent').'" />
+		<form method="post" action="options.php">
+			<? settings_fields('atbar_options'); ?>
+			<input type="hidden" name="atbar_persistent" value="<? get_option('atbar_persistent') ?>" />
 		</form>	
-	</table>');
+
+	</table>
+<?
+	
 }
 
 ?>
